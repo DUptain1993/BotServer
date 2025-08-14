d35e78d514b11e1496ea309d88c2e13490543fa9
DUptain1993 <daniel.r.uptain93@gmail.com>
2025-08-14 08:32:39 -0700

    First commit of Next.js application

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  }, 
-
-  images: {
-    unoptimized: true,
+  experimental: {
+    cpus: 1, 
   },
-};
+}; 
 
 module.exports = nextConfig;
 

013a9d013eabb924769cc776140679981f203cb4
DUptain1993 <mrfkry93@gmail.com>
2025-08-20 22:22:22 +0000

    6

---
+++ android/app/src/main/java/com/hackerbot/matrix/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hackerbot.matrix;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.hackerbot.matrix',
   appName: 'Hacker Bot Matrix',
-  webDir: '.next',
+  webDir: 'out',
   server: {
     androidScheme: 'https',
     cleartext: true,

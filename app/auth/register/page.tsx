5405f6d476c1c06a7deddfb241a82a6d42dded4d
DUptain1993 <mrfkry93@gmail.com>
2025-08-20 21:35:54 +0000

    1

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -25,7 +25,7 @@ export default async function RegisterPage() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-900 px-4 py-12 sm:px-6 lg:px-8">
-      <RegisterForm />
+      <RegisterForm onToggleMode={() => {}} />
     </div>
   )
 }
